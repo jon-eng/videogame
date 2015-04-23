6071a3e3431dac5c67818b0d9f5427d3a3f67116
Jonathan Eng <jon.kh.eng@gmail.com>
2015-04-23 18:37:36 -0400

    heroku1

--- models/index.js
+++ models/index.js
@@ -8,9 +8,9 @@ var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-if (process.env.HEROKU_POSTGRESQL_AQUA_URL) {
+if (process.env.HEROKU_POSTGRESQL_IVORY_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_AQUA_URL, {
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_IVORY_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
       logging:  true //false
